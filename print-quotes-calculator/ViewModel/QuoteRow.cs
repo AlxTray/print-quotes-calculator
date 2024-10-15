4f2c8dd492166213ae12d3ce5b6b09674034dd42
AlxTray <alex.ashby02@icloud.com>
2024-10-15 23:47:03 +0100

    feat: added ViewModel infrastructure

---
+++ print-quotes-calculator/ViewModel/QuoteRow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_quotes_calculator.ViewModel
+{
+    class QuoteRow : IQuoteRow
+    {
+        private string _material;
+        private double _materialUsage;
+        private string _ink;
+        private double _inkUsage;
+        private string _description;
+        private double _quoteCost;
+
+        public string Material 
+        { 
+            get => _material;
+            set 
+            { 
+                _material = value;
+                CalculateQuoteCost();
+                RaisePropertyChanged(nameof(Material));
+            } 
+        }
+        public double MaterialUsage 
+        { 
+            get => _materialUsage;
+            set 
+            { 
+                _materialUsage = value;
+                CalculateQuoteCost();
+                RaisePropertyChanged(nameof(MaterialUsage));
+            }
+        }
+        public string Ink 
+        { 
+            get => _ink;
+            set
+            {
+                _ink = value;
+                CalculateQuoteCost();
+                RaisePropertyChanged(nameof(Ink));
+            }
+        }
+        public double InkUsage 
+        { 
+            get => _inkUsage;
+            set
+            {
+                _inkUsage = value;
+                CalculateQuoteCost();
+                RaisePropertyChanged(nameof(InkUsage));
+            }
+        }
+        public string Description 
+        { 
+            get => _description;
+            set
+            {
+                _description = value;
+                RaisePropertyChanged(nameof(Description));
+            } 
+        }
+        public double QuoteCost 
+        { 
+            get => _quoteCost;
+        }
+        private void CalculateQuoteCost()
+        {
+            // Calculate the quote cost
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- print-quotes-calculator/ViewModel/QuotesViewModel.cs
+++ print-quotes-calculator/ViewModel/QuotesViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Unity;
 
 namespace print_quotes_calculator.ViewModel
 {
     internal class QuotesViewModel : IQuotesViewModel
     {
+        private UnityContainer _container;
+        private ObservableCollection<QuoteRow> _quoteRows;
+
+        public QuotesViewModel(UnityContainer container)
+        {
+            _container = container;
+            _quoteRows = [];
+            AddCommand = new RelayCommand(AddNewQuoteRow);
+        }
+
+        public ICommand AddCommand { get; }
+
+        public void addQuoteRow()
+        {
+            _quoteRows.Add(_container.Resolve<QuoteRow>());
+        }
     }
 }
